839fbefd793fe72ed505789569c96a8e4a6aa26c
surong <lxdyran@qq.com>
2016-05-12 18:24:31 +0800

    reverevert "input path"
    This reverts commit 6115a95965c666522127994bfa8ac47547b4f725.

--- drs/xmltest.py
+++ drs/xmltest.py
@@ -6,9 +6,11 @@ root_path = os.path.split(os.path.realpath(__file__))[0]
 input_path = root_path + '/Data/input/initplace'
 
 tree = ET.parse(input_path + '/test30/xml2.xml')
+
 print tree
 # fill the xml file here
-tree.write(input_path + '/test31/xml2.xml')
+
+tree.write(root_path + '/test31/xml2.xml')
 
 drs_retcode, drs_output = commands.getstatusoutput("echo '1 31 0' | " + root_path + "/drs03")
 print drs_retcode
