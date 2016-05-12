71e780f7405837f054aa8b65c063ef90db7628da
surong <lxdyran@qq.com>
2016-05-12 18:16:12 +0800

    xml and drs

--- drs/xmltest.py
+++ drs/xmltest.py
@@ -1,12 +1,17 @@
 import xml.etree.ElementTree as ET
 import os
+import commands
 
-root_path = os.path.split(os.path.realpath(__file__))[0] + '/Data/input/initplace'
+root_path = os.path.split(os.path.realpath(__file__))[0]
+input_path = root_path + '/Data/input/initplace'
 
-
-tree = ET.parse(root_path + '/test30/xml2.xml')
-
+tree = ET.parse(input_path + '/test30/xml2.xml')
 
 print tree
+# fill the xml file here
 
 tree.write(root_path + '/test31/xml2.xml')
+
+drs_retcode, drs_output = commands.getstatusoutput("echo '1 31 0' | " + root_path + "/drs03")
+print drs_retcode
+print drs_output
