2aecbcfb14d8dad43d5347e6faf881ebd8e808a1
surong <lxdyran@qq.com>
2016-05-12 10:58:28 +0800

    add /

--- drs/xmltest.py
+++ drs/xmltest.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import os
 
 root_path = os.path.split(os.path.realpath(__file__))[0]
-file_path = root_path + 'Data/input/initplace/test30/xml2.xml'
+file_path = root_path + '/Data/input/initplace/test30/xml2.xml'
 
 tree = ET.parse(file_path)
 
