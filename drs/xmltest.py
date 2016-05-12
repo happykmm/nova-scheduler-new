285bebbe0a2c0b1a0245a995876224b8b0b84f43
surong <lxdyran@qq.com>
2016-05-12 19:10:58 +0800

    no problem

--- drs/xmltest.py
+++ drs/xmltest.py
@@ -5,13 +5,13 @@ import commands
 root_path = os.path.split(os.path.realpath(__file__))[0]
 input_path = root_path + '/Data/input/initplace'
 
-tree = ET.parse(input_path + '/test30/xml2.xml')
+tree = ET.parse(input_path + '/sample/xml2.xml')
 
 print tree
 # fill the xml file here
 
-tree.write(input_path + '/test31/xml2.xml')
+tree.write(input_path + '/test30/xml2.xml')
 
-drs_retcode, drs_output = commands.getstatusoutput("echo '1 31 0' | " + root_path + "/drs03")
+drs_retcode, drs_output = commands.getstatusoutput("echo '1 30 0' | " + root_path + "/drs03")
 print drs_retcode
 print drs_output
